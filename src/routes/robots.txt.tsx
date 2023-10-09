770d37dc8c76d6395057b5c0cb017e7ae58189e8
Jay Pokale <jay.pokale.35@gmail.com>
2023-10-09 13:13:28 +0530

    Update robots.txt.tsx

--- src/routes/robots.txt.tsx
+++ src/routes/robots.txt.tsx
@@ -1,11 +1,12 @@
+import { A } from "solid-start";
+
 const Robots = () => {
   return (
     <div>
       <p>User-agent: *</p>
       <p>Allow: /*</p>
       <p>Disallow: /api/*</p>
-      <p>Sitemap: <a href={`sitemap.xml`}>
-        {`${import.meta.env.VITE_MAIN_URI}sitemap.xml`}</a>
+      <p>Sitemap: <A href="/sitemap.xml">{`${import.meta.env.VITE_MAIN_URI}sitemap.xml`}</A>
       </p>
     </div>
   );
