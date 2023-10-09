7af8eb16471857e63afcfded51ed16e12436d475
Jay Pokale <jay.pokale.35@gmail.com>
2023-10-09 13:06:40 +0530

    Update robots.txt.tsx

--- src/routes/robots.txt.tsx
+++ src/routes/robots.txt.tsx
@@ -4,7 +4,9 @@ const Robots = () => {
       <p>User-agent: *</p>
       <p>Allow: /*</p>
       <p>Disallow: /api/*</p>
-      <p>Sitemap: https://domain/sitemap.xml</p>
+      <p>Sitemap: <a href={`https:/${import.meta.env.VITE_MAIN_URI}}/sitemap.xml`}>
+        {`https:/${import.meta.env.VITE_MAIN_URI}}/sitemap.xml`}</a>
+      </p>
     </div>
   );
 };
